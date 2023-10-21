f704ae460885db60b0a771009176aad88a55ec75
paccvvet <120043593+paccvvet@users.noreply.github.com>
2023-10-21 14:24:22 +0300

    done

--- js/main.js
+++ js/main.js
@@ -19,3 +19,10 @@ function open(evt){
     tabTarget.classList.add('tabs__btn-item--active');
     document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
 }
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu--active');
+});
